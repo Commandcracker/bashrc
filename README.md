6e2672d6a4a6888d6a0a6e4473bae3368b507e3f
Commandcracker <lukas.Fricke.dev@gmail.com>
2022-08-02 22:46:51 +0200

    installer

---
+++ README.md
@@ -0,0 +1,5 @@
+# bashrc
+
+```bash
+source <(curl -s https://raw.githubusercontent.com/Commandcracker/bashrc/main/install.sh)
+```
--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@
 if [ ! -f ~/.git-prompt.sh ]; then
     wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
 fi
+
+wget https://raw.githubusercontent.com/Commandcracker/bashrc/main/src/.bashrc -O ~/.bashrc
+wget https://raw.githubusercontent.com/Commandcracker/bashrc/main/src/.bash_profile -O ~/.bash_profile
+wget https://raw.githubusercontent.com/Commandcracker/bashrc/main/src/.bash_aliases -O ~/.bash_aliases
+wget https://raw.githubusercontent.com/Commandcracker/bashrc/main/src/.bash_login -O ~/.bash_login
